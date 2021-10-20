d161bd42aa369ef3e4ed0493e8f47357b84fd1c5
injiri <simoninjiri@gmail.com>
2021-10-20 23:18:21 +0300

    #f-string

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
-def main():
-  print('You rolled a die')
+import random
 
-if __name__== "__main__":
-  main()
+
+def main():
+    roll = random.randint(1, 6)
+
+    print(f'You rolled a {roll}')  # f-string enables us to print variables into the string
+
+
+if __name__ == "__main__":
+    main()
